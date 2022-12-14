75ab5900a01a2430528c65d8c45b9fd1fffe8665
JoaoVVBP <joao.vitor.vbp@gmail.com>
2022-12-13 22:30:30 -0300

    Started a demo Spring Boot project

---
+++ src/main/java/com/example/tabia_internship/TabiaInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabia_internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabiaInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabiaInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tabia_internship/TabiaInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabia_internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabiaInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
